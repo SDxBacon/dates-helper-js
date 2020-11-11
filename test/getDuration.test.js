e487b24361bbb785b3da31cb7b30f097cc04bd2d
gwa <qqppgwa@gamil.com>
2020-11-11 11:23:12 +0800

    feat: 🎸 add getThisYear in getDuration.js

--- test/getDuration.test.js
+++ test/getDuration.test.js
@@ -1,45 +1,52 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, test } from '@jest/globals';
 import {
   getToday as oldGetToday,
   getYesterday as oldGetYesterday,
   getThisWeek as oldGetThisWeek,
   getThisMonth as oldGetThisMonth,
   getThisQuarter as oldGetThisQuarter,
-} from "../OriginDate.js";
+  getThisYear as oldGetThisYear,
+} from '../OriginDate.js';
 import {
   getToday,
   getYesterday,
   getThisWeek,
   getThisMonth,
   getThisQuarter,
-} from "../dates/index.js";
+  getThisYear,
+} from '../dates/index.js';
 
 /**
  * getDuration 測項：相對應的舊與新method輸出結果相符即可
  */
-describe("Testing `dates/duration/getDuration.js` methods.", function () {
+describe('Testing `dates/duration/getDuration.js` methods.', function () {
   /** getToday */
-  test("getToday", function () {
+  test('getToday', function () {
     expect(oldGetToday()).toEqual(getToday());
   });
 
   /** getYesterday */
-  test("getYesterday", function () {
+  test('getYesterday', function () {
     expect(oldGetYesterday()).toEqual(getYesterday());
   });
 
   /** getThisWeek */
-  test("getThisWeek", function () {
+  test('getThisWeek', function () {
     expect(oldGetThisWeek()).toEqual(getThisWeek());
   });
 
   /** getThisMonth */
-  test("getThisMonth", function () {
+  test('getThisMonth', function () {
     expect(oldGetThisMonth()).toEqual(getThisMonth());
   });
 
   /** getThisQuarter */
-  test("getThisQuarter", function () {
+  test('getThisQuarter', function () {
     expect(oldGetThisQuarter()).toEqual(getThisQuarter());
+  });
+
+  /** getThisYear */
+  test('getThisYear', function () {
+    expect(oldGetThisYear()).toEqual(getThisYear());
   });
 });
